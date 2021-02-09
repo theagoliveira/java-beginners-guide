0df36577ad9c1c7a12f375c39ef4c6bad206d81a
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-09 19:27:26 -0300

    Chapter 2, Program 15

---
+++ c2_p15/src/DtoL.java
@@ -0,0 +1,12 @@
+// *** This program will not compile. ***
+class DtoL {
+    public static void main(String[] args) {
+        long L;
+        double D;
+
+        D = 100123285.0;
+        L = D;
+
+        System.out.println("L and D: " + L + " " + D);
+    }
+}
