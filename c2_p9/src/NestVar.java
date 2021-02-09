609e5b286a27423e04353939ed8dff6447af82b2
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-09 18:58:25 -0300

    Chapter 2, Program 9

---
+++ c2_p9/src/NestVar.java
@@ -0,0 +1,17 @@
+/**
+ * NestVar
+ */
+public class NestVar {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 10; count++) {
+            System.out.println("This is count: " + count);
+
+            int count; // illegal!!!
+            for (count = 0; count < 2; count++) {
+                System.out.println("This program is in error!");
+            }
+        }
+    }
+}
