d7dbaafcb5c7bbc2b843eb5cd53bc11cdab0a3f2
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-09 18:42:16 -0300

    Chapter 2, Program 6

---
+++ c2_p6/src/DynInit.java
@@ -0,0 +1,13 @@
+/**
+ * DynInit
+ */
+public class DynInit {
+    public static void main(String[] args) {
+        double radius = 4, height = 5;
+
+        // dynamically initialize volume
+        double volume = 3.1416 * radius * radius * height;
+
+        System.out.println("Volume is " + volume);
+    }
+}
