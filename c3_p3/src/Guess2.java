5c4d9d83476425837e8e00e2aed0bab0c92dbe4d
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-09 23:55:35 -0300

    Chapter 3, Program 3

---
+++ c3_p3/src/Guess2.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+// Guess the letter game, 2nd version.
+public class Guess2 {
+    public static void main(String[] args) throws IOException {
+        char ch, answer = 'K';
+
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.print("Can you guess it: ");
+
+        ch = (char) System.in.read(); // read a char from the keyboard
+
+        if (ch == answer)
+            System.out.println("** Right **");
+        else
+            System.out.println("...Sorry, you're wrong.");
+    }
+}
