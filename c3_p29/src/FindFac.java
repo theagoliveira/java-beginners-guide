61df45e1b5d290db897ee0ce76db26a0f761fb96
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-11 00:34:16 -0300

    Chapter 3, Program 29 -- Extra challenge

--- c3_p29/src/FindFac.java
+++ c3_p29/src/FindFac.java
@@ -5,7 +5,7 @@ public class FindFac {
     public static void main(String[] args) {
         for (int i = 2; i <= 100; i++) {
             System.out.print("Factors of " + i + ": ");
-            for (int j = 2; j < i; j++)
+            for (int j = 2; j <= i / 2; j++)
                 if ((i % j) == 0)
                     System.out.print(j + " ");
             System.out.println();
