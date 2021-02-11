5e7e44f54677cbb37b1a7f77270d0749994adcb6
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-11 03:13:14 -0300

    Chapter 4, Program 7

---
+++ c4_p7/src/CompFuel.java
@@ -0,0 +1,50 @@
+// A program that uses the Vehicle class.
+
+class Vehicle {
+    int passengers; // number of passengers
+    int fuelcap; // fuel capacity in gallons
+    int mpg; // fuel consumption in miles per gallon
+
+    // Return the range.
+    int range() {
+        return mpg * fuelcap;
+    }
+
+    double fuelneeded(int miles) {
+        return (double) miles / mpg;
+    }
+}
+
+
+// This class declares an object of type Vehicle.
+public class CompFuel {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+        double gallons;
+        int dist = 252;
+
+
+        // assign values to fields in minivan
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // assign values to fields in sportscar
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        gallons = minivan.fuelneeded(dist);
+
+        System.out
+                .println("To go " + dist + " miles minivan needs " + gallons + " gallons of fuel.");
+
+        gallons = sportscar.fuelneeded(dist);
+
+        System.out.println(
+                "To go " + dist + " miles sportscar needs " + gallons + " gallons of fuel.");
+
+
+    }
+}
