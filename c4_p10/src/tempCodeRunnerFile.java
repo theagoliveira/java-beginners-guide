d56cae7deef305546a7e7ae36bde4c03f1f9e936
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-11 03:22:55 -0300

    Chapter 4, Program 10

--- c4_p10/src/tempCodeRunnerFile.java
+++ c4_p10/src/tempCodeRunnerFile.java
@@ -5,6 +5,13 @@ class Vehicle {
     int fuelcap; // fuel capacity in gallons
     int mpg; // fuel consumption in miles per gallon
 
+    // This is a constructor for Vehicle.
+    Vehicle(int p, int f, int m) {
+        passengers = p;
+        fuelcap = f;
+        mpg = m;
+    }
+
     // Return the range.
     int range() {
         return mpg * fuelcap;
@@ -17,23 +24,13 @@ class Vehicle {
 
 
 // This class declares an object of type Vehicle.
-public class CompFuel {
+public class VehConsDemo {
     public static void main(String[] args) {
-        Vehicle minivan = new Vehicle();
-        Vehicle sportscar = new Vehicle();
+        // construct complete vehicles
+        Vehicle minivan = new Vehicle(7, 16, 21);
+        Vehicle sportscar = new Vehicle(2, 14, 12);
         double gallons;
         int dist = 252;
-
-
-        // assign values to fields in minivan
-        minivan.passengers = 7;
-        minivan.fuelcap = 16;
-        minivan.mpg = 21;
-
-        // assign values to fields in sportscar
-        sportscar.passengers = 2;
-        sportscar.fuelcap = 14;
-        sportscar.mpg = 12;
 
         gallons = minivan.fuelneeded(dist);
 
