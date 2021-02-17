b20433c26e5c349d4e10e69ec8d1c40edfffcd93
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-16 22:48:03 -0300

    Chapter 8, Program 5

---
+++ c8_p5/src/SeriesDemo.java
@@ -0,0 +1,44 @@
+// Implement Series.
+class ByTwos implements Series {
+    int start;
+    int val;
+
+    ByTwos() {
+        start = 0;
+        val = 0;
+    }
+
+    public int getNext() {
+        val += 2;
+        return val;
+    }
+
+    public void reset() {
+        val = start;
+    }
+
+    public void setStart(int x) {
+        start = x;
+        val = x;
+    }
+}
+
+
+public class SeriesDemo {
+    public static void main(String[] args) {
+        var ob = new ByTwos();
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+
+        System.out.println("\nResetting");
+        ob.reset();
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+
+        System.out.println("\nStarting at 100");
+        ob.setStart(100);
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+    }
+}
