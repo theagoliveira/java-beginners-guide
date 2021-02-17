40fe8c5e6a7a3368a9efd734a73b5b077e1bda23
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-17 16:04:55 -0300

    Chapter 9, Program 14

---
+++ c9_p14/src/MultiCatch.java
@@ -0,0 +1,25 @@
+// Use the multi-catch feature. Note: This code requires JDK 7 or later to compile.
+class MultiCatch {
+    public static void main(String[] args) {
+        int a = 88;
+        int b = 0;
+        int result;
+        char[] chrs = {'A', 'B', 'C'};
+
+        for (int i = 0; i < 2; i++) {
+            try {
+                if (i == 0) {
+                    result = a / b; // generate an ArithmeticException
+                } else {
+                    chrs[5] = 'X'; // generate and ArrayIndexOutOfBoundsException
+                }
+            }
+            // This catch clause catches both exceptions.
+            catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Exception caught: " + e);
+            }
+
+            System.out.println("After multi-catch.");
+        }
+    }
+}
