07b7fc794dffacefde8c4add436a8349b048cdee
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-16 22:53:20 -0300

    Chapter 8, Program 7

---
+++ c8_p7/src/SeriesDemo.java
@@ -0,0 +1,44 @@
+// Implement Series.
+class ByThrees implements Series {
+    int start;
+    int val;
+
+    ByThrees() {
+        start = 0;
+        val = 0;
+    }
+
+    public int getNext() {
+        val += 3;
+        return val;
+    }
+
+    public void reset() {
+        val = start;
+    }
+
+    public void setStart(int x) {
+        start = x;
+        val = x;
+    }
+}
+
+
+public class SeriesDemo {
+    public static void main(String[] args) {
+        var ob = new ByThrees();
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+
+        System.out.println("\nResetting");
+        ob.reset();
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+
+        System.out.println("\nStarting at 100");
+        ob.setStart(100);
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next value is " + ob.getNext());
+    }
+}
