af2837f30cfe05de077bb10ed1f030b980503e87
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-20 00:51:05 -0300

    Chapter 10, Program 9

---
+++ c10_p9/src/RandomAccessDemo.java
@@ -0,0 +1,43 @@
+// Demonstrate random access files.
+
+import java.io.*;
+
+public class RandomAccessDemo {
+    public static void main(String[] args) {
+        double data[] = {19.4, 10.1, 123.54, 33.0, 87.9, 74.25};
+        double d;
+
+        // Open and use a random access file.
+        try (var raf = new RandomAccessFile("random.dat", "rw")) {
+            // Write values to the file.
+            for (int i = 0; i < data.length; i++) {
+                raf.writeDouble(data[i]);
+            }
+
+            // Now, read back specific values
+            raf.seek(0); // seek to first double
+            d = raf.readDouble();
+            System.out.println("First value is " + d);
+
+            raf.seek(8); // seek to second double
+            d = raf.readDouble();
+            System.out.println("Second value is " + d);
+
+            raf.seek((long) 8 * 3); // seek to fourth double
+            d = raf.readDouble();
+            System.out.println("Fourth value is " + d);
+
+            System.out.println();
+
+            // Now, read every other value.
+            System.out.println("Here is every other value: ");
+            for (int i = 0; i < data.length; i += 2) {
+                raf.seek((long) 8 * i); // seek to ith double
+                d = raf.readDouble();
+                System.out.print(d + " ");
+            }
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+}
