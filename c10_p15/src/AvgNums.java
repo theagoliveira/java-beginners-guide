0eeb4f4f3e2214d9343ff7f20723d8e3574b2f0a
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-20 01:54:43 -0300

    Chapter 10, Program 15

---
+++ c10_p15/src/AvgNums.java
@@ -0,0 +1,39 @@
+/* This program averages a list of numbers entered by the user. */
+
+import java.io.*;
+
+class AvgNums {
+    public static void main(String[] args) throws IOException {
+        // create a BufferedReader using System.in
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int n;
+        double sum = 0.0;
+        double avg;
+        double t;
+
+        System.out.print("How many numbers will you enter: ");
+        str = br.readLine();
+        try {
+            n = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid format");
+            return;
+        }
+
+        System.out.println("Enter " + n + " values.");
+        for (int i = 0; i < n; i++) {
+            System.out.print(": ");
+            str = br.readLine();
+            try {
+                t = Double.parseDouble(str);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid format");
+                t = 0.0;
+            }
+            sum += t;
+        }
+        avg = sum / n;
+        System.out.println("Average is " + avg);
+    }
+}
