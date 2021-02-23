6485354a79187bc40e8023b6a1ef145ef7385c3f
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-23 01:44:12 -0300

    Chapter 11, Program 4

---
+++ c11_p4/src/MoreThreads.java
@@ -0,0 +1,54 @@
+// Create multiple threads.
+
+class MyThread implements Runnable {
+    Thread thrd;
+
+    // Construct a new thread.
+    MyThread(String name) {
+        thrd = new Thread(this, name);
+    }
+
+    // A factory method that creates and starts a thread.
+    public static MyThread createAndStart(String name) {
+        var myThrd = new MyThread(name);
+
+        myThrd.thrd.start(); // start the thread
+        return myThrd;
+    }
+
+    // Entry point of thread.
+    public void run() {
+        System.out.println(thrd.getName() + " starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + thrd.getName() + ", count is " + count);
+            }
+        } catch (Exception e) {
+            System.out.println(thrd.getName() + " interrupted.");
+        }
+        System.out.println(thrd.getName() + " terminating.");
+    }
+}
+
+
+// Use isAlive().
+class MoreThreads {
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
+
+        var mt1 = MyThread.createAndStart("Child #1");
+        var mt2 = MyThread.createAndStart("Child #2");
+        var mt3 = MyThread.createAndStart("Child #3");
+
+        do {
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted.");
+            }
+        } while (mt1.thrd.isAlive() || mt2.thrd.isAlive() || mt3.thrd.isAlive());
+        System.out.println("Main thread ending.");
+    }
+}
