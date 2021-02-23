43c9e7be56003810c4ca904bc85fa3f164bcfdb1
Thiago Cavalcante <thiago.kun@gmail.com>
2021-02-23 00:57:41 -0300

    Chapter 11, Program 2

---
+++ c11_p2/src/ThreadVariations.java
@@ -0,0 +1,54 @@
+// MyThread variations. This version of MyThread creates a Thread when its constructor is called and
+// stores it in an instance variable called thrd. It also sets the name of the thread and provides a
+// factory method to create and start a thread.
+
+class MyThread implements Runnable {
+    Thread thrd;
+
+    // Construct a new thread using this Runnable and give it a name.
+    MyThread(String name) {
+        thrd = new Thread(this, name);
+    }
+
+    // A factory method that creates and starts a thread.
+    public static MyThread createAndStart(String name) {
+        MyThread myThrd = new MyThread(name);
+
+        myThrd.thrd.start();
+        return myThrd;
+    }
+
+    // Entry point of thread
+    public void run() {
+        System.out.println(thrd.getName() + " starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + thrd.getName() + ", count is " + count);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(thrd.getName() + " interrupted.");
+        }
+        System.out.println(thrd.getName() + " terminating.");
+    }
+}
+
+
+public class ThreadVariations {
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
+
+        // Create and start a thread.
+        MyThread mt = MyThread.createAndStart("Child #1");
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted.");
+            }
+        }
+        System.out.println("Main thread ending.");
+    }
+}
