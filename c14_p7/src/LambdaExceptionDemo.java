efdfa5733616f0e302d7a2df3f60531c8093b1e5
Thiago Cavalcante <thiago.kun@gmail.com>
2021-03-09 02:52:23 -0300

    Chapter 14, Program 7

---
+++ c14_p7/src/LambdaExceptionDemo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+interface MyIOAction {
+    boolean ioAction(Reader rdr) throws IOException;
+}
+
+
+public class LambdaExceptionDemo {
+    public static void main(String[] args) {
+        // This block lambda could throw an IOException.
+        // Thus, IOException must be specified in a throws
+        // clause of ioAction() in MyIOAction.
+        MyIOAction myIO = (rdr) -> {
+            int ch = rdr.read(); // could throw IOException
+            // ...
+            return true;
+        };
+    }
+}
